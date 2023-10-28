f8da586ef0b6967b963b8ad73384da50347c4877
KERRCAM <90578826+KERRCAM@users.noreply.github.com>
2023-10-28 19:21:44 +0100

    flowy

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ class Particle {
         this.speedModifier = Math.floor(Math.random() * 5 + 1);
         this.history = [{x: this.x, y: this.y}]; 
         this.maxLength = Math.floor(Math.random() * 200 + 10); 
-        this.angle = 0;
+        this.angle = 0; 
+        this.timer = this.maxLength * 2;
     } 
     
     draw(context){ 
@@ -32,22 +33,36 @@ class Particle {
         context.stroke();
     } 
 
-    update() {   
-        let x = Math.floor(this.x / this.effect.cellSize); 
-        let y = Math.floor(this.y / this.effect.cellSize);  
-        let index = y * this.effect.cols + x;
-        this.angle = this.effect.flowField[index]; 
-
-        //can change to manipulate movement
-        this.speedX = Math.cos(this.angle); 
-        this.speedY = Math.sin(this.angle);
-        this.x += this.speedX * this.speedModifier; 
-        this.y += this.speedY * this.speedModifier;
-        
-        this.history.push({x: this.x, y: this.y});
-        if (this.history.length > this.maxLength){ 
+    update() {    
+        this.timer--; //every animation tick timmer decreases by one 
+        if (this.timer >= 1){ 
+            let x = Math.floor(this.x / this.effect.cellSize); 
+            let y = Math.floor(this.y / this.effect.cellSize);  
+            let index = y * this.effect.cols + x;
+            this.angle = this.effect.flowField[index]; 
+    
+            //can change to manipulate movement
+            this.speedX = Math.cos(this.angle); 
+            this.speedY = Math.sin(this.angle);
+            this.x += this.speedX * this.speedModifier; 
+            this.y += this.speedY * this.speedModifier;
+            
+            this.history.push({x: this.x, y: this.y});
+            if (this.history.length > this.maxLength){ 
+                this.history.shift();
+            } 
+        } else if (this.history.length > 1){ 
             this.history.shift();
+        } else { 
+            this.reset();
         }
+    } 
+
+    reset(){ 
+        this.x = Math.floor(Math.random() * this.effect.width); 
+        this.y = Math.floor(Math.random() * this.effect.height);  
+        this.history = [{x: this.x, y: this.y}]; 
+        this.timer = this.maxLength * 2;
     }
 } 
 
@@ -56,13 +71,13 @@ class Effect {
         this.width = width; 
         this.height = height; 
         this.particles = []; 
-        this.numberOfParticles = 300;  
-        this.cellSize = 20; 
+        this.numberOfParticles = 1000;  
+        this.cellSize = 5; // can reduce to make smoother flow
         this.rows; 
         this.cols; 
         this.flowField = [];  
-        this.curve = 0.5; 
-        this.zoom = 0.15;
+        this.curve = 1.0; 
+        this.zoom = 0.11;
         this.init();
     } 
 
